4a2fee4db2bba95bd9c98b534b6216a052f1b888
Ethan <ethanaaronbailey@gmail.com>
2024-03-22 12:23:34 +0000

    Started on input validation for DTM

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 class state:
-    def __init__(self,label,operations):
+    def __init__(self,label,operations,valid):
         #operations is a 2D list that contains lists of the format
         #[input symbol,read, write, move read tape, move write tape]
         #where input symbol, read and writ are terminals,
@@ -7,6 +7,7 @@ class state:
         #or "N" (None)
         self.label=label
         self.operations=operations
+        self.valid=valid  #Checks the state implies a valid input
         
         
 class terminal:
@@ -20,12 +21,20 @@ class tape:
 class readTape(tape):
     def __init__(self,state):
         super().__init__(state)
-
+        self.index=0
+    
 class writeTape(tape):
     def __init__(self):
         super().__init__()
+        self.index=1
 
 class DTM:
     def __init__(self,states,start):
         self.states=states
         self.start=start
+
+    def checkInput(self,inp):
+        currentRead = readTape(list(input))
+        currentWrite = writeTape()
+        for operation in self.states[self.states.index(self.start)].operations:
+            pass
