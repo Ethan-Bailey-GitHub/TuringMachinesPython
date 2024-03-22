976f34775b11d58e1c97032a2a05fde934edb2f4
Ethan <ethanaaronbailey@gmail.com>
2024-03-22 12:40:14 +0000

    Added test data

--- main.py
+++ main.py
@@ -1,14 +1,17 @@
 class state:
     def __init__(self,label,operations,valid):
         #operations is a 2D list that contains lists of the format
-        #[input symbol,read, write, move read tape, move write tape]
+        #[input symbol, read, write, move read tape, move write tape, nextState]
         #where input symbol, read and writ are terminals,
         #move read tape and move write tape are either "L" (left), "R" (Right)
-        #or "N" (None)
+        #or None (None)
         self.label=label
         self.operations=operations
         self.valid=valid  #Checks the state implies a valid input
-        
+
+    def changeNextState(self,operationIndex,nextState):
+        self.operations[operationIndex][5]=nextState
+
         
 class terminal:
     def __init__(self,label):
@@ -34,7 +37,12 @@ class DTM:
         self.start=start
 
     def checkInput(self,inp):
-        currentRead = readTape(list(input))
+        currentRead = readTape(list(inp))
         currentWrite = writeTape()
         for operation in self.states[self.states.index(self.start)].operations:
             pass
+
+S1=state("S1",(("x",None,None,"R",None,None),(">",None,None,None,None,None)),False) #S1 has one operation: x, ∆/∆, R, N
+S1.changeNextState(0,S1)  #S1 now goes to itself in its first operation
+S2=state("S2",(),True)
+S1.changeNextState(1,S2) #S1 now goes to the valid state in its second operation
